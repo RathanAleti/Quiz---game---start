2ea34ebf215cb295777faddf0d5d22215e3ee98d
RathanAleti <aletirathanreddy77@gmail.com>
2025-08-07 10:52:42 -0500

    Created a question_bank with an empty list to add the questions from new_question object every time the for loop iterates in question_data list

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from question_model import Question
 from data import question_data
 
+question_bank = []
+for questions in question_data:
+    question_text = questions["text"]
+    question_answer = questions["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+print(question_bank)
