8aea410bee38f60aadefa4d0b29e109628ae9d30
RathanAleti <aletirathanreddy77@gmail.com>
2025-08-07 15:44:33 -0500

    In QuizBrain class I have created a new method called still_has_questions .I created this method to check is there any questions left to ask. I have used a return function to give t/f .

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,6 +4,9 @@ class QuizBrain:
         self.questions_number = 0
         self.questions_list = questions_list
 
+    def still_has_questions(self):
+        return self.questions_number < len(self.questions_list)
+
     def next_question(self,):
         current_question = self.questions_list[self.questions_number]
         self.questions_number += 1
