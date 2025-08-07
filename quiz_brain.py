a64f38bf2e16cef24354cb0246b106ef923ca361
RathanAleti <aletirathanreddy77@gmail.com>
2025-08-07 14:48:39 -0500

    In Quiz_brain file I have created a new class QuizBrain then created a method which has input of questions_list then created attributes one of them is questions_number attribute and given a default value 0.For second attribute I have named it questions_list. Created a method called next_question and created a new variable called current_question which retrieves an item from question_list. Increasing the questions_number value by 1 so that every time it iterates through the for loop the question number value gets increased.

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+
+    def __init__(self,questions_list):
+        self.questions_number = 0
+        self.questions_list = questions_list
+
+    def next_question(self,):
+        current_question = self.questions_list[self.questions_number]
+        self.questions_number += 1
+        input(f"Q.{self.questions_number}: {current_question.text} (True/False): ")
